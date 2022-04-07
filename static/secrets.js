1d39aca779291f9426f9ab485fb5107154ef39bc
Prima <primabreuerjenkins@gmail.com>
2022-04-07 10:07:33 -0500

    delete unnecessary files

--- static/secrets.js
+++ static/secrets.js
@@ -1,5 +1,6 @@
 const apiKey = '2bca2b98eb9f47b486bd02d72febbf9b';
 
 const SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin@localhost:5432/whats_for_dinner';
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://postgres:admin@localhost:5432/whats_for_dinner').replace('postgres://', 'postgresql://')
 
 export {apiKey};
